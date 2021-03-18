c496caff331c5ca96cb2cf73d5b58dcd2ca01ce6
Matthew Collier <mcollier@digitalbazaar.com>
2021-03-18 12:06:54 -0400

    Add zcap to config.

--- test/mocha/20-http-insert.js
+++ test/mocha/20-http-insert.js
@@ -12,9 +12,15 @@ const nsBaseUrl = `${bedrock.config.server.baseUri}/ns`;
 describe('namespace HTTP insert API', () => {
   it('should create a namespace', async () => {
     const controller = 'urn:uuid:a2a530e9-788b-4e7d-ad5e-865bc4078ef8';
+
+    const zcap = {
+      id: 'urn:uuid:011d784b-19ba-4a80-9cb3-bb1c2749148c',
+    };
+
     const config = {
       controller,
       sequence: 0,
+      zcap,
     };
 
     let err;
@@ -30,9 +36,10 @@ describe('namespace HTTP insert API', () => {
     }
     assertNoError(err);
     should.exist(result);
-    result.data.should.have.keys(['controller', 'id', 'sequence']);
+    result.data.should.have.keys(['controller', 'id', 'sequence', 'zcap']);
     result.data.controller.should.equal(controller);
     result.data.sequence.should.equal(0);
     result.data.id.should.be.a('string');
+    result.data.zcap.should.eql(zcap);
   });
 });
